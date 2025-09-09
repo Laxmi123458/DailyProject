ba6ab7ff4291f5bfa7c102dc75c9836da6aeccf8
Laxmi123458 <laxmikumari.080294@gmail.com>
2025-09-09 14:52:21 +0200

    git commit -m "Login Feature updated modified code"

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
 from pageObjects.loginPage import Login
+from testCases import conftest
 
 class Test_Logins:
     BaseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 
-    def test_valid_login(self):
-        driver=webdriver.Chrome()
-        driver.maximize_window()
+    def test_valid_login(self,setup):
+        driver=setup
         driver.get(self.BaseUrl)
         login_Page=Login(driver)
         login_Page.enter_username("Admin")
